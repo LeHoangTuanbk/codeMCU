5abdbf843c327d83d1a76d468d919ee37ef53022
thegreatman <lehoangtuanbk@gmail.com>
2020-06-29 22:09:51 +0700

    code LCD

---
+++ LCDnodeMCU.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal_I2C.h>
+
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+
+  Serial.begin(115200);
+  
+  //Use predefined PINS consts
+  
+  Wire.begin(D2,D1); // chi có 2 chân D2, D1 là hỗ trợ SPI
+  lcd.backlight();
+  lcd.init();
+  
+  lcd.home();
+  
+  lcd.print("Hello, NodeMCU");
+
+}
+void loop()
+{ // do nothing here 
+  }
